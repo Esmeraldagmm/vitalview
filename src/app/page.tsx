2c38369e00aa1112643d256ff59064c456d23e02
JNikolo <125705821+JNikolo@users.noreply.github.com>
2025-03-09 04:11:52 -0400

    fix: Route links fix to navigate the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Link } from "lucide-react";
 
 import LandingPage from "@/app/LandingPage";
-import ReportPage from "@/app/ReportPage";
+import ReportPage from "@/app/report/page";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <ReportPage />
-      {/* <LandingPage /> */}
+      {/* <ReportPage /> */}
+      <LandingPage />
     </div>
   );
 }
